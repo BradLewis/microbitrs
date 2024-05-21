46a698335485be8050404124030915a38e51527a
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2024-05-20 21:09:40 -0400

    Add basic Hello World using the BSP package

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,195 @@
 #![no_std]
 #![no_main]
 
-use cortex_m::asm::nop;
-use panic_halt as _; 
+use panic_halt as _;
 
 use cortex_m_rt::entry;
-use rtt_target::{rprintln, rtt_init_print};
+use embedded_hal::delay::DelayNs;
+use microbit::{board::Board, display::blocking::Display, hal::Timer};
 
 #[entry]
 fn main() -> ! {
-    rtt_init_print!();
-    rprintln!("Hello, world!");
+    if let Some(board) = Board::take() {
+        let mut timer = Timer::new(board.TIMER0);
+        let mut display = Display::new(board.display_pins);
 
-    loop {
-        rprintln!("Looping...");
-        for _ in 0..100_000 {
-            nop();
+        #[allow(non_snake_case)]
+        let letter_O = [
+            [0, 0, 1, 0, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+            [0, 0, 1, 0, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_S = [
+            [0, 0, 1, 1, 0],
+            [0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 1, 0],
+            [0, 1, 1, 0, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_R = [
+            [0, 1, 1, 0, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 1, 0, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_B = [
+            [0, 1, 1, 0, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 1, 0, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 1, 0, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_A = [
+            [0, 0, 1, 0, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 1, 1, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_D = [
+            [0, 1, 1, 0, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 1, 0, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_U = [
+            [0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+            [0, 0, 1, 0, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_P = [
+            [0, 1, 1, 0, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 1, 0, 0],
+            [0, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_E = [
+            [0, 1, 1, 1, 0],
+            [0, 1, 0, 0, 0],
+            [0, 1, 1, 1, 0],
+            [0, 1, 0, 0, 0],
+            [0, 1, 1, 1, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_N = [
+            [1, 0, 0, 0, 1],
+            [1, 1, 0, 0, 1],
+            [1, 0, 1, 0, 1],
+            [1, 0, 0, 1, 1],
+            [1, 0, 0, 0, 1],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_I = [
+            [0, 1, 1, 1, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 1, 1, 1, 0],
+        ];
+
+        let heart = [
+            [0, 1, 0, 1, 0],
+            [1, 0, 1, 0, 1],
+            [1, 0, 0, 0, 1],
+            [0, 1, 0, 1, 0],
+            [0, 0, 1, 0, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_J = [
+            [0, 0, 1, 1, 0],
+            [0, 0, 0, 1, 0],
+            [0, 0, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+            [0, 0, 1, 0, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_M = [
+            [1, 0, 0, 0, 1],
+            [1, 1, 0, 1, 1],
+            [1, 0, 1, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+        ];
+        #[allow(non_snake_case)]
+        let letter_T = [
+            [0, 1, 1, 1, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_W = [
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1],
+            [0, 1, 0, 1, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_L = [
+            [0, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0],
+            [0, 1, 1, 1, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_H = [
+            [0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 1, 1, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+        ];
+
+
+        loop {
+            timer.delay_ms(5000);
+            display.show(&mut timer, letter_H, 1000);
+            display.show(&mut timer, letter_E, 1000);
+            display.show(&mut timer, letter_L, 1000);
+            display.show(&mut timer, letter_L, 1000);
+            display.show(&mut timer, letter_O, 1000);
+            display.show(&mut timer, letter_W, 1000);
+            display.show(&mut timer, letter_O, 1000);
+            display.show(&mut timer, letter_R, 1000);
+            display.show(&mut timer, letter_L, 1000);
+            display.show(&mut timer, letter_D, 1000);
+            display.clear();
+            timer.delay_ms(250_u32);
         }
     }
-}
 
+    panic!("End");
+}
