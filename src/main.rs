15c6af553850b0ec650f6fdabe496817383bb37b
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2024-09-14 20:50:59 -0400

    Display temperature of the chip on the LEDs

--- src/main.rs
+++ src/main.rs
@@ -4,192 +4,178 @@
 use panic_halt as _;
 
 use cortex_m_rt::entry;
-use embedded_hal::delay::DelayNs;
-use microbit::{board::Board, display::blocking::Display, hal::Timer};
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{Temp, Timer},
+};
+use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
     if let Some(board) = Board::take() {
         let mut timer = Timer::new(board.TIMER0);
         let mut display = Display::new(board.display_pins);
 
-        #[allow(non_snake_case)]
-        let letter_O = [
-            [0, 0, 1, 0, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 0, 1, 0],
-            [0, 0, 1, 0, 0],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_S = [
-            [0, 0, 1, 1, 0],
-            [0, 1, 0, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 0, 1, 0],
-            [0, 1, 1, 0, 0],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_R = [
-            [0, 1, 1, 0, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 1, 0, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 0, 1, 0],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_B = [
-            [0, 1, 1, 0, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 1, 0, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 1, 0, 0],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_A = [
-            [0, 0, 1, 0, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 1, 1, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 0, 1, 0],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_D = [
-            [0, 1, 1, 0, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 1, 0, 0],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_U = [
-            [0, 1, 0, 1, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 0, 1, 0],
-            [0, 0, 1, 0, 0],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_P = [
-            [0, 1, 1, 0, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 1, 0, 0],
-            [0, 1, 0, 0, 0],
-            [0, 1, 0, 0, 0],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_E = [
-            [0, 1, 1, 1, 0],
-            [0, 1, 0, 0, 0],
-            [0, 1, 1, 1, 0],
-            [0, 1, 0, 0, 0],
-            [0, 1, 1, 1, 0],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_N = [
-            [1, 0, 0, 0, 1],
-            [1, 1, 0, 0, 1],
-            [1, 0, 1, 0, 1],
-            [1, 0, 0, 1, 1],
-            [1, 0, 0, 0, 1],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_I = [
-            [0, 1, 1, 1, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 1, 1, 1, 0],
-        ];
-
-        let heart = [
-            [0, 1, 0, 1, 0],
-            [1, 0, 1, 0, 1],
-            [1, 0, 0, 0, 1],
-            [0, 1, 0, 1, 0],
-            [0, 0, 1, 0, 0],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_J = [
-            [0, 0, 1, 1, 0],
-            [0, 0, 0, 1, 0],
-            [0, 0, 0, 1, 0],
-            [0, 1, 0, 1, 0],
-            [0, 0, 1, 0, 0],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_M = [
-            [1, 0, 0, 0, 1],
-            [1, 1, 0, 1, 1],
-            [1, 0, 1, 0, 1],
-            [1, 0, 0, 0, 1],
-            [1, 0, 0, 0, 1],
-        ];
-        #[allow(non_snake_case)]
-        let letter_T = [
-            [0, 1, 1, 1, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_W = [
-            [1, 0, 0, 0, 1],
-            [1, 0, 0, 0, 1],
-            [1, 0, 1, 0, 1],
-            [1, 0, 1, 0, 1],
-            [0, 1, 0, 1, 0],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_L = [
-            [0, 1, 0, 0, 0],
-            [0, 1, 0, 0, 0],
-            [0, 1, 0, 0, 0],
-            [0, 1, 0, 0, 0],
-            [0, 1, 1, 1, 0],
-        ];
-
-        #[allow(non_snake_case)]
-        let letter_H = [
-            [0, 1, 0, 1, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 1, 1, 0],
-            [0, 1, 0, 1, 0],
-            [0, 1, 0, 1, 0],
-        ];
-
-
+        let mut temp = Temp::new(board.TEMP);
         loop {
-            timer.delay_ms(5000);
-            display.show(&mut timer, letter_H, 1000);
-            display.show(&mut timer, letter_E, 1000);
-            display.show(&mut timer, letter_L, 1000);
-            display.show(&mut timer, letter_L, 1000);
-            display.show(&mut timer, letter_O, 1000);
-            display.show(&mut timer, letter_W, 1000);
-            display.show(&mut timer, letter_O, 1000);
-            display.show(&mut timer, letter_R, 1000);
-            display.show(&mut timer, letter_L, 1000);
-            display.show(&mut timer, letter_D, 1000);
+            rprintln!("Reading temperature");
+            let deg_c: f32 = temp.measure().to_num();
+            rprintln!("Temperature: {}°C", deg_c);
+
+            let led_display = get_display(deg_c);
+
+            display.show(&mut timer, led_display, 10000);
+
             display.clear();
-            timer.delay_ms(250_u32);
         }
     }
 
     panic!("End");
+}
+
+fn get_display(temp: f32) -> [[u8; 5]; 5] {
+    let digit = (temp % 10.0) as i32;
+    let mut display = match digit {
+        0 => [
+            [0, 0, 1, 1, 1],
+            [0, 0, 1, 0, 1],
+            [0, 0, 1, 0, 1],
+            [0, 0, 1, 0, 1],
+            [0, 0, 1, 1, 1],
+        ],
+        1 => [
+            [0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 1],
+        ],
+        2 => [
+            [0, 0, 1, 1, 1],
+            [0, 0, 0, 0, 1],
+            [0, 0, 1, 1, 1],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 1, 1],
+        ],
+        3 => [
+            [0, 0, 1, 1, 1],
+            [0, 0, 0, 0, 1],
+            [0, 0, 1, 1, 1],
+            [0, 0, 0, 0, 1],
+            [0, 0, 1, 1, 1],
+        ],
+        4 => [
+            [0, 0, 1, 0, 1],
+            [0, 0, 1, 0, 1],
+            [0, 0, 1, 1, 1],
+            [0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 1],
+        ],
+        5 => [
+            [0, 0, 1, 1, 1],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 1, 1],
+            [0, 0, 0, 0, 1],
+            [0, 0, 1, 1, 1],
+        ],
+        6 => [
+            [0, 0, 1, 1, 1],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 1, 1],
+            [0, 0, 1, 0, 1],
+            [0, 0, 1, 1, 1],
+        ],
+        7 => [
+            [0, 0, 1, 1, 1],
+            [0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 1],
+        ],
+        8 => [
+            [0, 0, 1, 1, 1],
+            [0, 0, 1, 0, 1],
+            [0, 0, 1, 1, 1],
+            [0, 0, 1, 0, 1],
+            [0, 0, 1, 1, 1],
+        ],
+        9 => [
+            [0, 0, 1, 1, 1],
+            [0, 0, 1, 0, 1],
+            [0, 0, 1, 1, 1],
+            [0, 0, 0, 0, 1],
+            [0, 0, 1, 1, 1],
+        ],
+        _ => [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ],
+    };
+
+    let tens = (temp / 10.0) as i32;
+    match tens {
+        1 => {
+            display[0][0] = 1;
+        }
+        2 => {
+            display[0][0] = 1;
+            display[0][1] = 1;
+        }
+        3 => {
+            display[0][0] = 1;
+            display[0][1] = 1;
+            display[1][0] = 1;
+        }
+        4 => {
+            display[0][0] = 1;
+            display[0][1] = 1;
+            display[1][0] = 1;
+            display[1][0] = 1;
+        }
+        _ => {}
+    };
+
+    let decimal = ((temp * 10.0) % 10.0) as i32;
+    match decimal {
+        1 => {
+            display[4][1] = 1;
+        }
+        2 => {
+            display[4][0] = 1;
+        }
+        3 => {
+            display[4][0] = 1;
+            display[4][1] = 1;
+        }
+        4 => {
+            display[3][1] = 1;
+        }
+        5 => {
+            display[3][1] = 1;
+            display[4][1] = 1;
+        }
+        6 => {
+            display[3][1] = 1;
+            display[4][0] = 1;
+        }
+        7 => {
+            display[3][1] = 1;
+            display[4][0] = 1;
+            display[4][1] = 1;
+        }
+        8 => {
+            display[3][0] = 1;
+        }
+        9 => {
+            display[3][0] = 1;
+            display[4][1] = 1;
+        }
+        _ => {}
+    }
+    display
 }
